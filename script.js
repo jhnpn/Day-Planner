2a5ba892276dcea06918e7a8db8e60e5f5cdbd63
umaiibo <j.hnpn123@gmail.com>
2020-03-30 01:03:17 -0700

    pseudo code

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// 
+$(document).ready(function() {
+    // listen for the save button
+
+    // create local storage
+
+    // find a way to update the hours
+    // create a for loop for hour blocks
+
+    // find a way to display current day
+
+    // load saved data from local stroage
+}) 
