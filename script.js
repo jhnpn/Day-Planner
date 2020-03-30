df1bcde53efa35ef87b3362169556bef86d097f8
umaiibo <j.hnpn123@gmail.com>
2020-03-30 15:19:12 -0700

    created event listener for save button as well as local storage

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ $(document).ready(function() {
     // listen for the save button
     // create local storage
     $(".saveBtn").on("click", function() {
-        // var time = 
+        var time = $(this).parent().attr("id");
+        var description = $(this).siblings(".description").val();
+
+        localStorage.setItem(time, description);
     })
 
     // find a way to update the hours
     // create a for loop for hour blocks
-
+    function hourUpdate() {
+        var currentHour = moment().hours();
+    }
     // find a way to display current day
     $("#current-day").text(moment().format("ddd, MMMM Do, YYYY"));
     // load saved data from local stroage
