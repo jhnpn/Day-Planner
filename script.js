8086e677cce047453299b53be2877bf9deeec762
umaiibo <j.hnpn123@gmail.com>
2020-03-30 12:50:08 -0700

    created a way to display the current date in the header

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
+    let m = moment();
+    console.log(moment())
     // listen for the save button
-
     // create local storage
+    $(".saveBtn").on("click", function() {
+        // var time = 
+    })
 
     // find a way to update the hours
     // create a for loop for hour blocks
 
     // find a way to display current day
-
+    $("#current-day").text(moment().format("ddd, MMMM Do, YYYY"));
     // load saved data from local stroage
 }) 
